2bfc5551000f7ad0bb806c20d4bbdec7d3d03271
Kevin Chan <kevchan85@yahoo.com>
2020-05-17 22:59:46 -0700

    fixed get-properties.sh

--- get-properties.sh
+++ get-properties.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-source ./common.sh
 if [ ! -e .jwt ] || [ ! -e .url ] ; then
   ./login.sh
 fi
@@ -13,10 +12,9 @@ if [ -z "${THING}" ]; then
   exit 1
 fi
 
-PROPERTIES_URL=${URL}/things/${THING}/properties
 
 curl -H "Authorization:Bearer ${JWT}" \
  -H "Content-Type: application/json" \
  -H "Accept: application/json" \
  --insecure --silent \
- ${PROPERTIES_URL} | python -mjson.tool
+ ${URL}/things/${THING}/properties | python -mjson.tool
